2f81151bbfd4bcc0c9ccb00c02c625ddfdf7a9cc
Manuel Bahamóndez-Honores <manuel@b9.cl>
2026-02-06 18:51:15 -0300

    Bump version to 0.8.2

--- beancount_chile/__init__.py
+++ beancount_chile/__init__.py
@@ -3,6 +3,6 @@
 from beancount_chile.banco_chile import BancoChileImporter
 from beancount_chile.banco_chile_credit import BancoChileCreditImporter
 
-__version__ = "0.8.1"
+__version__ = "0.8.2"
 
 __all__ = ["BancoChileImporter", "BancoChileCreditImporter"]
